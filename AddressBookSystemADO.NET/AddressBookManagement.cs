e506a82a227e68dabea59c53f85e55e059a51b94
Shubham Seth <shubhamsth1234@gmail.com>
2022-01-30 10:35:07 +0530

    [add] Shubham added UC2 of AddressBookSystemADO.NET program

--- AddressBookSystemADO.NET/AddressBookManagement.cs
+++ AddressBookSystemADO.NET/AddressBookManagement.cs
@@ -37,5 +37,65 @@ namespace AddressBookSystemADO.NET
             }
 
         }
+
+        /* UC2:- Ability to create a Address Book Table with first and last names, 
+                 address, city, state, zip, phone number and email as its attributes 
+        */
+
+        public void GetAllContact()
+        {
+
+            AddressBookModel model = new AddressBookModel();
+            try
+            {
+                using (connection)
+                {
+                    string query = @"select * from dbo.AddressBookSystem"; // Query to get all the data from the table
+
+                    // Impementing the command on the connection fetched database table
+                    SqlCommand command = new SqlCommand(query, connection); 
+
+                    connection.Open();  //Open the connection.
+
+                    SqlDataReader reader = command.ExecuteReader();  // executing the sql data reader to fetch the records
+                    if (reader.HasRows)
+                    {
+
+                        while (reader.Read())  // Mapping the data to the employee model class object
+                        {
+                            model.FirstName = reader.GetString(0);
+                            model.LastName = reader.GetString(1);
+                            model.Address = reader.GetString(2);
+                            model.City = reader.GetString(3);
+                            model.State = reader.GetString(4);
+                            model.Zip = reader.GetString(5);
+                            model.PhoneNumber = reader.GetString(6);
+                            model.EmailId = reader.GetString(7);
+                            model.AddressBookType = reader.GetString(8);
+                            model.AddressBookName = reader.GetString(9);
+                            Console.WriteLine("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}", model.FirstName, model.LastName,
+                                model.Address, model.City, model.State, model.Zip, model.PhoneNumber, model.EmailId, model.AddressBookType, model.AddressBookName);
+                            
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No Records Found in Address Book System Table");
+                    }
+                    reader.Close();
+                }
+            }
+
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            finally
+            {
+                connection.Close(); // Always ensuring the closing of the connection
+            }
+        }
+
     }
 }
